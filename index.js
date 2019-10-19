9dc5386a13336c3b4bd7a283891c9b01cd53e9d5
Kyle Gillingham <kylegillingham362@gmail.com>
2019-10-19 15:26:34 -0230

    Initial commit, basic scaffolding.

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ *  Parse and validate event. 
+ * @param {*} event AWS lambda handler event
+ * @returns empty object
+ */
+const validateEvent = (event) => {
+    // Dummy function for design
+    return{}
+}
+
+
+export const handler = async (event) => {
+    // Parse and validate event
+    const charParams = validateEvent(event)
+    
+    // Determine Race
+
+    // Determine other attributes
+
+    // Return to user
+}
+
