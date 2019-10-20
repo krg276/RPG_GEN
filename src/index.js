43d62a1d112c50d11249e9c52ac802cabe626512
Devin <devthedevel@gmail.com>
2019-10-20 10:51:35 -0230

    Updated project structure
    - Moved code to src directory
    - Updated .gitignore
    - Added serverless framework
    - Added sample http request data

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@
  * @returns empty object
  */
 const validateEvent = (event) => {
+    console.log('Validating event...')
+
+    console.log('Event validated!')
     // Dummy function for design
     return{}
 }
 
 
-export const handler = async (event) => {
+module.exports.handler = async (event) => {
     // Parse and validate event
     const charParams = validateEvent(event)
     
@@ -18,5 +21,6 @@ export const handler = async (event) => {
     // Determine other attributes
 
     // Return to user
+    console.log('Character generated!')
 }
 
